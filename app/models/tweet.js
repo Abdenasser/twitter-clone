79a20eed8624539db4934195a69721f3dc9e9336
abdenasser <nasser.elidrissi065@gmail.com>
2016-11-13 23:54:22 +0000

    writing some validations for our Tweet

--- app/models/tweet.js
+++ app/models/tweet.js
@@ -7,4 +7,6 @@ class Tweet extends Nodal.Model {}
 Tweet.setDatabase(Nodal.require('db/main.js'));
 Tweet.setSchema(Nodal.my.Schema.models.Tweet);
 
+Tweet.validates('body', 'must be at least 5 characters', v => v && v.length >= 5);
+
 module.exports = Tweet;
