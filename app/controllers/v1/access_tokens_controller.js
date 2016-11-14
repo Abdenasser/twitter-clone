77f8bee63d049e83a72b45cac0d11c62d25b2b3b
abdenasser <nasser.elidrissi065@gmail.com>
2016-11-14 01:44:55 +0000

    handle login logic in access_token controller

--- app/controllers/v1/access_tokens_controller.js
+++ app/controllers/v1/access_tokens_controller.js
@@ -4,59 +4,13 @@ const Nodal = require('nodal');
 const AccessToken = Nodal.require('app/models/access_token.js');
 
 class V1AccessTokensController extends Nodal.Controller {
-
-  index() {
-
-    AccessToken.query()
-      .where(this.params.query)
-      .end((err, models) => {
-
-        this.respond(err || models);
-
-      });
-
-  }
-
-  show() {
-
-    AccessToken.find(this.params.route.id, (err, model) => {
-
-      this.respond(err || model);
-
-    });
-
-  }
-
   create() {
 
-    AccessToken.create(this.params.body, (err, model) => {
-
-      this.respond(err || model);
-
+    AccessToken.login(this.params, (err, accessToken) => {
+      this.respond(err || accessToken);
     });
-
+    
   }
-
-  update() {
-
-    AccessToken.update(this.params.route.id, this.params.body, (err, model) => {
-
-      this.respond(err || model);
-
-    });
-
-  }
-
-  destroy() {
-
-    AccessToken.destroy(this.params.route.id, (err, model) => {
-
-      this.respond(err || model);
-
-    });
-
-  }
-
 }
 
 module.exports = V1AccessTokensController;
